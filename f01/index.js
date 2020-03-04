7101445feb17ad3f282860fa0c86b38fea4d2120
Leanxp <leanxp1995@gmail.com>
2020-03-04 16:16:54 +0000

    Finalizado el F01

---
+++ f01/index.js
@@ -0,0 +1,19 @@
+console.log("========================= [Hola, soy la tarea 13] =========================");
+
+//Nos vamos creando el servidor web...
+const express = require("express");
+var app = express();
+
+//Esto significa que crea un servidor web escuchando en el puerto 80, y cuando alguien acceda al recurso "/cool" le va a devolver una carita feliz.
+/**app.get("/cool",(req, response) => {
+	response.send("<html>"+cool()+"</html>");
+})**/
+
+//Nos vamos a hacer ahora para cuando pidamos el recurso "/date"
+
+app.get("/date", (req, response) => {
+	var  date = new Date();
+	response.send("<html>"+date+"</html>")
+})
+app.listen(80);
+console.log("server ready");
