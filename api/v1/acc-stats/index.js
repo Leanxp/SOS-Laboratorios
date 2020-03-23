2ec9fefc6b9c0dce1b89da73734c04c8a49de46b
Leanxp <leanxp1995@gmail.com>
2020-03-23 16:23:55 +0000

    Hola

--- api/v1/acc-stats/index.js
+++ api/v1/acc-stats/index.js
@@ -7,53 +7,59 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 80; //Si no nos dice nada, desplegamos en puerto 80 y si no, pues el puerto que diga HEROKU.
 
-var contacts = [
+var accstats = [
 	{ 
-		name: "Leandro",
-		phone: 123456	
+		province: "Madrid",
+		year: 2014,
+		accvictotal: 19942,
+		accvicinter: 4324,
+		accfall: 114
 	},
 	{ 
-		name: "pablo",
-		phone: 789456	
+		province: "Sevilla",
+		year: 2018,
+		accvictotal: 6515,
+		accvicinter: 1945,
+		accfall: 58	
 	}
 ];
 
 const BASE_API_URL = "/api/v1"; //Tendremos aqui la URL Base, la api el path...
 
 ////////////////////////////////////////////////////
-// GET CONTACTS   
+// GET accstats   
 ////////////////////////////////////////////////////
 
-app.get(BASE_API_URL+"/contacts", (req,res) =>{ //En req vienen los datos de la petición.
-	res.send(JSON.stringify(contacts,null,2)); //En JSON devuelvo todos los contactos
-	console.log("Data sent: "+JSON.stringify(contacts, null, 2));
+app.get(BASE_API_URL+"/accstats", (req,res) =>{ //En req vienen los datos de la petición.
+	res.send(JSON.stringify(accstats,null,2)); //En JSON devuelvo todos los contactos
+	console.log("Data sent: "+JSON.stringify(accstats, null, 2));
 });
 
 
 ////////////////////////////////////////////////////
-// POST CONTACTS   
+// POST accstats   
 ////////////////////////////////////////////////////
 
-app.post(BASE_API_URL+"/contacts",(req,res) =>{
-	//contacts.push(req.body);  //En req me vienen los datos de la petición, cojo el body también
+app.post(BASE_API_URL+"/accstats",(req,res) =>{
+	//accstats.push(req.body);  //En req me vienen los datos de la petición, cojo el body también
 	//Verificaremos ahora que es un contacto
-	var newContact = req.body;
-	if((newContact == "") || (newContact.name == null)){  //Si está vacío o es nulo
+	var newAccstats = req.body;
+	if((newAccstats == "") || (newAccstats.province == null)){  //Si está vacío o es nulo
 		res.sendStatus(400, "BAD REQUEST");
 	}else{
-		contacts.push(newContact);
+		accstats.push(newAccstats);
 		res.sendStatus(201,"CREATED"); 
 	}
 });
 
 ////////////////////////////////////////////////////
-// DELETE CONTACTS   
+// DELETE accstats   
 ////////////////////////////////////////////////////
 
 
-app.delete(BASE_API_URL+ "/contacts", (req,res) =>{
-	contacts = ["YOU DON'T HAVE ANY CONTACTS"];
-	res.sendStatus(200, "CONTACTS DELETED");
+app.delete(BASE_API_URL+ "/accstats", (req,res) =>{
+	accstats = ["YOU DON'T HAVE ANY accstats"];
+	res.sendStatus(200, "accstats DELETED");
 })
 
 
@@ -64,21 +70,21 @@ app.delete(BASE_API_URL+ "/contacts", (req,res) =>{
 //  ========> AHORA SOBRE ELEMENTOS EN CONCRETO...
 
 ////////////////////////////////////////////////////
-// GET CONTACTS/XXX Es decir, a un recurso en concreto   
+// GET accstats/XXX Es decir, a un recurso en concreto   
 ////////////////////////////////////////////////////
 
 
-app.get(BASE_API_URL+"/contacts/:name", (req,res)=>{ //El :name lo que hace es como que crea una variable que puede tener cualquier valor, yo puedo tener /:org /:loquesea yo puedo tener los que yo quiera
+app.get(BASE_API_URL+"/accstats/:name", (req,res)=>{ //El :name lo que hace es como que crea una variable que puede tener cualquier valor, yo puedo tener /:org /:loquesea yo puedo tener los que yo quiera
 	//¿Cómo accedería al valor concreto de lo que me ha llegado? Pues así con el params el nombre que yo le haya puesto en el : con param me lo pilla
 	var name = req.params.name;
 	
-	var filteredContacts = contacts.filter((c) => {
+	var filteredAccstats = accstats.filter((c) => {
 		//Le digo que solo me deje pasar en el caso de que el contacto tenga el mismo nombre que el que me están pasando
 		return (c.name == name);
 	});
 	
-	if(filteredContacts.length >= 1){
-		res.send(filteredContacts[0]); //Devolvería el primer elemento de ese array
+	if(filteredAccstats.length >= 1){
+		res.send(filteredAccstats[0]); //Devolvería el primer elemento de ese array
 	}else{
 		res.sendStatus(404, "CONTACT NOT FOUND");
 	}
@@ -88,14 +94,14 @@ app.get(BASE_API_URL+"/contacts/:name", (req,res)=>{ //El :name lo que hace es c
 
 
 ////////////////////////////////////////////////////
-// PUT CONTACTS/XXX Es decir, a un recurso en concreto   
+// PUT accstats/XXX Es decir, a un recurso en concreto   
 ////////////////////////////////////////////////////
 
-app.put(BASE_API_URL+"/contacts/:name", (req,res)=>{
+app.put(BASE_API_URL+"/accstats/:name", (req,res)=>{
 	var name = req.params.name;
 	var body = req.body;
 	
-	var updatedContacts = contacts.map((c) => {
+	var updatedAccstats = accstats.map((c) => {
 		var updatedC = c;
 		
 		if (c.name == name) {
@@ -106,29 +112,29 @@ app.put(BASE_API_URL+"/contacts/:name", (req,res)=>{
 		return (updatedC);
 	});
 	
-	if (updatedContacts.length == 0) {
+	if (updatedAccstats.length == 0) {
 		res.sendStatus(404, "CONTACT NOT FOUND");
 	} else {
-		contacts = updatedContacts;
+		accstats = updatedAccstats;
 		res.sendStatus(200, "OK");
 	}
 });
 
 
 ////////////////////////////////////////////////////
-// DELETE CONTACTS/XXX Es decir, a un recurso en concreto   
+// DELETE accstats/XXX Es decir, a un recurso en concreto   
 ////////////////////////////////////////////////////
 
-app.delete(BASE_API_URL+"/contacts/:name", (req,res)=>{ //Para el delete podría usar un filter pero quitando el que me llega
+app.delete(BASE_API_URL+"/accstats/:name", (req,res)=>{ //Para el delete podría usar un filter pero quitando el que me llega
 	var name = req.params.name;
 	
-	var filteredContacts = contacts.filter((c) => {
+	var filteredAccstats = accstats.filter((c) => {
 		return (c.name != name);
 	});
 	
 	
-	if(filteredContacts.length < contacts.length){
-		contacts = filteredContacts;
+	if(filteredAccstats.length < accstats.length){
+		accstats = filteredAccstats;
 		res.sendStatus(200);
 	}else{
 		res.sendStatus(404,"CONTACT NOT FOUND");
